20530f0962805f42a2ad5d8e88140add1188074d
zhs007 <sssxueren@gmail.com>
2019-03-27 00:06:32 +0800

    - updated format article

--- plugins/exportarticle/baijingapp.js
+++ plugins/exportarticle/baijingapp.js
@@ -36,9 +36,51 @@ async function proc(url, page) {
  */
 async function formatArticle(page) {
   await page.evaluate(() => {
+    const lstmh = $('.mod-head');
+    if (lstmh && lstmh.length > 0) {
+      const mh = lstmh[0];
+
+      mh.style.cssText = '';
+      mh.className = 'article-head';
+
+      let authorname = '';
+      const lstauthorname = $('span.name');
+      if (lstauthorname && lstauthorname.length > 0) {
+        authorname = lstauthorname[0].innerText;
+      }
+
+      let curtime = '';
+      const lstcurtime = $('span.time');
+      if (lstcurtime && lstcurtime.length > 0) {
+        const varreg = /([1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):[0-5]\d)/;
+        curtime = varreg.exec(lstcurtime[0].innerText)[0];
+      }
+
+      for (let i = 0; i < mh.childNodes.length; ) {
+        if (mh.childNodes[i].nodeName == '#text' ||
+        mh.childNodes[i].nodeName == '#comment' ||
+        mh.childNodes[i].nodeName == 'DIV') {
+          mh.childNodes[i].remove();
+        } else {
+          ++i;
+        }
+      }
+
+      const authotnode = document.createElement('p');
+      authotnode.innerHTML = '<p>' + authorname + '</p>';
+      mh.appendChild(authotnode);
+
+      const curtimenode = document.createElement('p');
+      curtimenode.innerHTML = '<p>' + curtime + '</p>';
+      mh.appendChild(curtimenode);
+    }
+
     const lstmb = $('.mod-body');
     if (lstmb && lstmb.length > 0) {
-      const mb = $('.mod-body')[0];
+      const mb = lstmb[0];
+
+      mb.style.cssText = '';
+      mb.className = 'article-body';
 
       for (let i = 0; i < mb.children.length; ) {
         if (mb.children[i].id != 'message') {

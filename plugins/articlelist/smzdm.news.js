a6cdb3644f3ad3098e9417a3156d0a97c57f400c
zhs007 <sssxueren@gmail.com>
2019-04-24 21:26:13 +0800

    - updated getarticles

---
+++ plugins/articlelist/smzdm.news.js
@@ -0,0 +1,66 @@
+const {mgrPlugins} = require('./pluginsmgr');
+// const {jarviscrawlercore} = require('../../proto/result');
+// const images = require('images');
+
+/**
+ * ismine
+ * @param {string} url - URL
+ * @return {bool} ismine
+ */
+function ismine(url) {
+  if (url == 'https://news.smzdm.com') {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * getArticles
+ * @param {object} page - page
+ * @return {ArticleList} result - result
+ */
+async function getArticles(page) {
+  let errret = undefined;
+  const ret = await page.evaluate(async () => {
+    const ret = {};
+    ret.articles = [];
+
+    const lst = $('.list');
+    for (let i = 0; i < lst.length; ++i) {
+      const title = lst[i].getElementsByClassName('listTitle');
+      if (title.length > 0) {
+        co = {
+          title: title[0].innerText,
+        };
+
+        const href = title[0].getElementsByTagName('a');
+        if (href.length > 0) {
+          co.url = href[0].href;
+        }
+
+        const lrInfo = lst[i].getElementsByClassName('lrInfo');
+        if (lrInfo.length > 0) {
+          co.summary = lrInfo.innerText;
+        }
+
+        ret.articles.push(co);
+      }
+    }
+
+    console.log(ret);
+
+    return ret;
+  }).catch((err) => {
+    console.log('iheima.main:getArticles.evaluate', err);
+
+    errret = err;
+  });
+
+  return {
+    result: ret,
+    err: errret,
+  };
+}
+
+mgrPlugins.regGetArticles('smzdm.news', ismine, getArticles);
---
+++ plugins/articlelist/smzdm.post.js
@@ -0,0 +1,60 @@
+const {mgrPlugins} = require('./pluginsmgr');
+// const {jarviscrawlercore} = require('../../proto/result');
+// const images = require('images');
+
+/**
+ * ismine
+ * @param {string} url - URL
+ * @return {bool} ismine
+ */
+function ismine(url) {
+  if (url == 'https://post.smzdm.com') {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * getArticles
+ * @param {object} page - page
+ * @return {ArticleList} result - result
+ */
+async function getArticles(page) {
+  let errret = undefined;
+  const ret = await page.evaluate(async () => {
+    const ret = {};
+    ret.articles = [];
+
+    const lst = $('.feed-block.z-hor-feed.post-feed-block');
+    for (let i = 0; i < lst.length; ++i) {
+      const title = lst[i].getElementsByClassName('z-feed-title');
+      if (title.length > 0) {
+        co = {
+          title: title[0].innerText,
+        };
+
+        const href = lst[i].getElementsByTagName('a');
+
+        co.url = href[0].href;
+
+        ret.articles.push(co);
+      }
+    }
+
+    console.log(ret);
+
+    return ret;
+  }).catch((err) => {
+    console.log('iheima.main:getArticles.evaluate', err);
+
+    errret = err;
+  });
+
+  return {
+    result: ret,
+    err: errret,
+  };
+}
+
+mgrPlugins.regGetArticles('smzdm.post', ismine, getArticles);

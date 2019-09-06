7d05bc927e492e39c975ff556c5693c87a2a0627
zhs007 <sssxueren@gmail.com>
2019-09-06 13:35:57 +0800

    - fixed a bug in proto

--- proto/result_pb.js
+++ proto/result_pb.js
@@ -15130,7 +15130,7 @@ proto.jarviscrawlercore.RequestSteepAndCheap.deserializeBinaryFromReader = funct
     var field = reader.getFieldNumber();
     switch (field) {
     case 1:
-      var value = /** @type {!proto.jarviscrawlercore.TechInAsiaMode} */ (reader.readEnum());
+      var value = /** @type {!proto.jarviscrawlercore.SteepAndCheapMode} */ (reader.readEnum());
       msg.setMode(value);
       break;
     case 2:
@@ -15195,15 +15195,15 @@ proto.jarviscrawlercore.RequestSteepAndCheap.serializeBinaryToWriter = function(
 
 
 /**
- * optional TechInAsiaMode mode = 1;
- * @return {!proto.jarviscrawlercore.TechInAsiaMode}
+ * optional SteepAndCheapMode mode = 1;
+ * @return {!proto.jarviscrawlercore.SteepAndCheapMode}
  */
 proto.jarviscrawlercore.RequestSteepAndCheap.prototype.getMode = function() {
-  return /** @type {!proto.jarviscrawlercore.TechInAsiaMode} */ (jspb.Message.getFieldWithDefault(this, 1, 0));
+  return /** @type {!proto.jarviscrawlercore.SteepAndCheapMode} */ (jspb.Message.getFieldWithDefault(this, 1, 0));
 };
 
 
-/** @param {!proto.jarviscrawlercore.TechInAsiaMode} value */
+/** @param {!proto.jarviscrawlercore.SteepAndCheapMode} value */
 proto.jarviscrawlercore.RequestSteepAndCheap.prototype.setMode = function(value) {
   jspb.Message.setProto3EnumField(this, 1, value);
 };

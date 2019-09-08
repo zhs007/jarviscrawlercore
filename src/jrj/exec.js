d7edbccd992d84bcba3a3628c7c95bd2ec4c05ca
zhs007 <sssxueren@gmail.com>
2019-09-08 19:35:38 +0800

    - added jrj.funds

---
+++ src/jrj/exec.js
@@ -0,0 +1,53 @@
+const {startBrowser} = require('../browser');
+const {jrjFunds} = require('./funds');
+
+/**
+ * execJRJ
+ * @param {object} program - program
+ * @param {string} version - version
+ */
+async function execJRJ(program, version) {
+  program
+      .command('jrj [mode]')
+      .description('jrj')
+      .option('-t, --timeout [timeout]', 'time out')
+      .option('-h, --headless [isheadless]', 'headless mode')
+      .action(function(mode, options) {
+        console.log('version is ', version);
+
+        if (!mode) {
+          console.log('command wrong, please type ' + 'jarviscrawler jrj --help');
+
+          return;
+        }
+
+        console.log('mode - ', mode);
+
+        let timeout = 3 * 60 * 1000;
+        if (typeof options.timeout == 'number') {
+          timeout = options.timeout;
+        }
+
+        const headless = options.headless === 'true';
+        console.log('headless - ', headless);
+
+        (async () => {
+          const browser = await startBrowser(headless);
+
+          if (mode == 'funds') {
+            const ret = await jrjFunds(browser, timeout);
+            console.log(JSON.stringify(ret));
+          }
+
+          await browser.close();
+        })().catch((err) => {
+          console.log('catch a err ', err);
+
+          if (headless) {
+            process.exit(-1);
+          }
+        });
+      });
+}
+
+exports.execJRJ = execJRJ;
---
+++ src/jrj/funds.js
@@ -0,0 +1,137 @@
+/**
+ * getCode - get code
+ * @param {string} name - name
+ * @return {string} code - code
+ */
+function getCode(name) {
+  const lstarr0 = name.split('【');
+  if (lstarr0.length == 2) {
+    const lstarr1 = lstarr0[1].split('】');
+    if (lstarr1.length == 2) {
+      return lstarr1[0];
+    }
+  }
+
+  return '';
+}
+
+/**
+ * jrjFunds - jrj funds
+ * @param {object} browser - browser
+ * @param {number} timeout - timeout in microseconds
+ * @return {object} ret - {error, ret}
+ */
+async function jrjFunds(browser, timeout) {
+  let awaiterr = undefined;
+  const page = await browser.newPage();
+
+  await page
+      .setViewport({
+        width: 1280,
+        height: 600,
+        deviceScaleFactor: 1,
+      })
+      .catch((err) => {
+        awaiterr = err;
+      });
+
+  if (awaiterr) {
+    console.log('jrjFunds.setViewport', awaiterr);
+
+    await page.close();
+
+    return {error: awaiterr.toString()};
+  }
+
+  // await page.setRequestInterception(true);
+  // page.on('request', async (req) => {
+  //   const rt = req.resourceType();
+  //   if (rt == 'image' || rt == 'media' || rt == 'font') {
+  //     await req.abort();
+
+  //     return;
+  //   }
+
+  //   await req.continue();
+  // });
+
+  // if (pageid > 1) {
+  //   url += '&page=' + (pageid - 1).toString();
+  // }
+
+  await page
+      .goto('http://fund.jrj.com.cn/family.shtml', {
+        timeout: timeout,
+        waitUntil: 'domcontentloaded',
+      })
+      .catch((err) => {
+        awaiterr = err;
+      });
+
+  if (awaiterr) {
+    console.log('jrjFunds.goto', awaiterr);
+
+    await page.close();
+
+    return {error: awaiterr.toString()};
+  }
+
+  await page.waitForSelector('div.box').catch((err) => {
+    awaiterr = err;
+  });
+
+  if (awaiterr) {
+    console.log('jrjFunds.waitForSelector div.box', awaiterr);
+
+    await page.close();
+
+    return {error: awaiterr.toString()};
+  }
+
+  const ret = await page
+      .$$eval('div.box', (eles) => {
+        console.log(eles);
+
+        if (eles.length > 0) {
+          eles = eles[0].getElementsByClassName('ln1');
+
+          const ret = [];
+
+          for (let i = 0; i < eles.length; ++i) {
+            const lsta = eles[i].getElementsByTagName('a');
+            for (let j = 0; j < lsta.length; ++j) {
+              ret.push(lsta[j].innerText);
+            }
+          }
+
+          return ret;
+        }
+
+        return [];
+      })
+      .catch((err) => {
+        awaiterr = err;
+      });
+
+  if (awaiterr) {
+    console.log('jrjFunds.$$eval div.box', awaiterr);
+
+    await page.close();
+
+    return {error: awaiterr.toString()};
+  }
+
+  await page.close();
+
+  const codes = [];
+  for (let i = 0; i < ret.length; ++i) {
+    const code = getCode(ret[i]);
+    if (code.length > 0) {
+      codes.push(code);
+    }
+  }
+
+  return {ret: {codes: codes}};
+}
+
+exports.jrjFunds = jrjFunds;

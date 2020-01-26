549047c55286783319aa80d5c2c443ce92f8a9c1
zerro <zerrozhao@gmail.com>
2020-01-26 12:16:26 +0800

    - updated index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const {startBrowser} = require('../src/browser');
+const {manhuadbManhua} = require('./src/manhuadb/manhua');
+const {manhuadbBook} = require('./src/manhuadb/book');
+
+exports.startBrowser = startBrowser;
+
+exports.manhuadbManhua = manhuadbManhua;
+exports.manhuadbBook = manhuadbBook;

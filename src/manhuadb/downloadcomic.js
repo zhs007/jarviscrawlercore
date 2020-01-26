1c43a99dbed81d17a4a485a4186ad2284f3bdec0
zerro <zerrozhao@gmail.com>
2020-01-26 20:35:45 +0800

    - updated manhuadb

--- src/manhuadb/downloadcomic.js
+++ src/manhuadb/downloadcomic.js
@@ -3,7 +3,8 @@ const {manhuadbManhua} = require('./manhua');
 const {manhuadbBook} = require('./book');
 const {parseBookURL} = require('./utils');
 const log = require('../log');
-const {download} = require('../request');
+const {DownloadList} = require('../request');
+const {sleep} = require('../utils');
 const fs = require('fs');
 const path = require('path');
 
@@ -15,13 +16,24 @@ const path = require('path');
  * @return {error} err - error
  */
 async function downloadComic(isdebug, comicid, rootpath) {
+  try {
+    fs.mkdirSync(rootpath);
+  } catch (err) {
+    log.error('downloadComic mkdirSync error ', err);
+  }
+
   const browser = await startBrowser(!isdebug);
-  const timeout = 3 * 60 * 1000;
+  const timeout = 30 * 1000;
+  const dl = new DownloadList();
 
   const manhuaret = await manhuadbManhua(browser, comicid, timeout);
   if (manhuaret.error) {
     log.error('downloadComic error ', manhuaret.error);
   }
+
+  (async () => {
+    await dl.run();
+  })();
 
   for (let i = 0; i < manhuaret.ret.books.length; ++i) {
     const curret = parseBookURL(manhuaret.ret.books[i].url);
@@ -31,10 +43,19 @@ async function downloadComic(isdebug, comicid, rootpath) {
         curret.bookid,
         path.join(rootpath, manhuaret.ret.books[i].name),
         timeout,
+        dl,
     );
     if (curbookret) {
       log.error('downloadBook error ', curbookret);
     }
+  }
+
+  while (true) {
+    if (dl.isFinished()) {
+      break;
+    }
+
+    sleep(1000);
   }
 
   return undefined;
@@ -47,47 +68,99 @@ async function downloadComic(isdebug, comicid, rootpath) {
  * @param {string} bookid - bookid
  * @param {string} rootpath - rootpath
  * @param {int} timeout - timeout
+ * @param {object} dl - DownloadList
  * @return {error} err - error
  */
-async function downloadBook(browser, comicid, bookid, rootpath, timeout) {
-  const lst = [];
-  const ret = await manhuadbBook(browser, comicid, bookid, 1, timeout);
-  if (ret.error) {
-    return ret.error;
+async function downloadBook(browser, comicid, bookid, rootpath, timeout, dl) {
+  try {
+    fs.mkdirSync(rootpath);
+  } catch (err) {
+    log.error('downloadBook mkdirSync error ', err);
   }
 
-  download(ret.ret.pages[0].url).then((ret) => {
+  let ret;
+  const lst = [];
+  while (true) {
+    ret = await manhuadbBook(browser, comicid, bookid, 1, timeout);
     if (ret.error) {
-      log.error(
-          'downloadBook.download ' + ret.pages[0].url + ' error ',
-          ret.error,
-      );
+      log.error('downloadBook.manhuadbBook error', ret.error);
+
+      await sleep(30 * 1000);
+
+      continue;
     }
 
-    fs.writeFileSync(path.join(rootpath, '1.jpg'), ret.buf);
-  });
+    dl.addTask(
+        ret.ret.pages[0].url,
+        (buf, param) => {
+          fs.writeFileSync(path.join(param.rootpath, param.pi + '.jpg'), buf);
+        },
+        {rootpath: rootpath, pi: 1},
+    );
 
-  lst.push(ret.ret.pages[0].url);
+    lst.push(ret.ret.pages[0].url);
+
+    break;
+  }
+
+  // const lsterr = [];
+  // let downloadnums = 0;
+  // download(ret.ret.pages[0].url).then((cdret) => {
+  //   if (cdret.error) {
+  //     log.error(
+  //         'downloadBook.download ' + ret.ret.pages[0].url + ' error ',
+  //         cdret.error,
+  //     );
+
+  //     lsterr.push(ret.ret.pages[0].url);
+
+  //     return;
+  //   }
+
+  //   fs.writeFileSync(path.join(rootpath, '1.jpg'), cdret.buf);
+  //   downloadnums++;
+  // });
+
+  // lst.push(ret.ret.pages[0].url);
   // fs.writeFileSync(path.join(rootpath, '1.jpg'), ret.pages[0].data);
 
   for (let i = 2; i <= ret.ret.pageNums; ++i) {
-    const curret = await manhuadbBook(browser, comicid, bookid, i, timeout);
-    if (curret.error) {
-      return curret.error;
-    }
+    while (true) {
+      const curret = await manhuadbBook(browser, comicid, bookid, i, timeout);
+      if (curret.error) {
+        log.error('downloadBook.manhuadbBook error', curret.error);
 
-    download(curret.ret.pages[0].url).then((ret) => {
-      if (ret.error) {
-        log.error(
-            'downloadBook.download ' + curret.pages[0].url + ' error ',
-            ret.error,
-        );
+        await sleep(30 * 1000);
+
+        continue;
       }
 
-      fs.writeFileSync(path.join(rootpath, i + '.jpg'), ret.buf);
-    });
+      dl.addTask(
+          curret.ret.pages[0].url,
+          (buf, param) => {
+            fs.writeFileSync(path.join(param.rootpath, param.pi + '.jpg'), buf);
+          },
+          {rootpath: rootpath, pi: i},
+      );
 
-    lst.push(curret.ret.pages[0].url);
+      lst.push(curret.ret.pages[0].url);
+
+      break;
+    }
+
+    // download(curret.ret.pages[0].url).then((ret) => {
+    //   if (ret.error) {
+    //     log.error(
+    //         'downloadBook.download ' + curret.pages[0].url + ' error ',
+    //         ret.error,
+    //     );
+    //   }
+
+    //   fs.writeFileSync(path.join(rootpath, i + '.jpg'), ret.buf);
+    //   downloadnums++;
+    // });
+
+    // lst.push(curret.ret.pages[0].url);
     // fs.writeFileSync(path.join(rootpath, i + '.jpg'), curret.pages[0].data);
   }
 
--- src/request.js
+++ src/request.js
@@ -1,4 +1,6 @@
 const request = require('request');
+const {sleep} = require('./utils');
+const log = require('./log');
 
 /**
  * download - download a file
@@ -18,22 +20,95 @@ function download(url, timeout) {
       }
 
       err = error;
-    }).on('data', (data) =>{
-      if (buf) {
-        buf = Buffer.concat([buf, data]);
-      } else {
-        buf = data;
-      }
-    }).on('end', () => {
-      if (err) {
-        resolve({error: err});
+    })
+        .on('data', (data) => {
+          if (buf) {
+            buf = Buffer.concat([buf, data]);
+          } else {
+            buf = data;
+          }
+        })
+        .on('end', () => {
+          if (err) {
+            resolve({error: err});
 
-        return;
-      }
+            return;
+          }
 
-      resolve({buf: buf});
-    });
+          resolve({buf: buf});
+        });
   });
 }
 
+/**
+ * DownloadList class
+ */
+class DownloadList {
+  /**
+   * constructor
+   */
+  constructor() {
+    this.lst = [];
+    this.timeout = 30 * 1000;
+  }
+
+  /**
+   * addTask
+   * @param {string} url - url
+   * @param {function} onfunc - func(buf, param)
+   * @param {object} param - param
+   */
+  addTask(url, onfunc, param) {
+    this.lst.push({url: url, onfunc: onfunc, param: param});
+  }
+
+  /**
+   * runImp
+   */
+  async runImp() {
+    while (true) {
+      if (this.lst.length > 0) {
+        const curtask = this.lst[0];
+        const ret = await download(curtask.url, this.timeout);
+        if (ret.error) {
+          log.error('DownloadList.download ' + curtask.url, ret.error);
+
+          this.lst.push(curtask);
+          this.lst.splice(0, 1);
+
+          await sleep(1000);
+
+          continue;
+        }
+
+        curtask.onfunc(ret.buf, curtask.param);
+
+        this.lst.splice(0, 1);
+      } else {
+        await sleep(1000);
+      }
+    }
+  }
+
+  /**
+   * run
+   * @return {Promise<undefined>} ret - return
+   */
+  run() {
+    return new Promise((resolve, reject) => {
+      this.runImp();
+      resolve();
+    });
+  }
+
+  /**
+   * isFinished
+   * @return {boolean} isFinished - isFinished
+   */
+  isFinished() {
+    return this.lst.length == 0;
+  }
+}
+
 exports.download = download;
+exports.DownloadList = DownloadList;
